021e50e660a8195f997a21af9ec477e8d3f3e86d
Bhavana Malli <mallibhavana3@gmail.com>
2022-11-16 17:16:02 -0600

    Added Aeroplane create,update and delete files

--- controllers/Aeroplane.js
+++ controllers/Aeroplane.js
@@ -92,3 +92,67 @@ exports.Aeroplane_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 }
+// Handle Aeroplane delete on DELETE.
+exports.Aeroplane_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Aeroplane.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+// Handle a show one view with id specified by query
+exports.Aeroplane_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Aeroplane.findById( req.query.id)
+    res.render('Aeroplanedetail',
+    { title: 'Aeroplane Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle building the view for creating a Aeroplane.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.Aeroplane_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('Aeroplanecreate', { title: 'Aeroplane Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle building the view for updating a Aeroplane.
+// query provides the id
+exports.Aeroplane_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Aeroplane.findById(req.query.id)
+    res.render('Aeroplaneupdate', { title: 'Aeroplane Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle a delete one view with id from query
+exports.Aeroplane_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Aeroplane.findById(req.query.id)
+    res.render('Aeroplanedelete', { title: 'Aeroplane Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/Aeroplane.js
+++ routes/Aeroplane.js
@@ -11,3 +11,11 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', Aeroplane_controlers.Aeroplane_view_all_Page );
 module.exports = router;
+/* GET detail Aeroplane page */
+router.get('/detail', Aeroplane_controlers.Aeroplane_view_one_Page);
+/* GET create Aeroplane page */
+router.get('/create', Aeroplane_controlers.Aeroplane_create_Page);
+/* GET create update page */
+router.get('/update', Aeroplane_controlers.Aeroplane_update_Page);
+/* GET delete Aeroplane page */
+router.get('/delete', Aeroplane_controlers.Aeroplane_delete_Page);
